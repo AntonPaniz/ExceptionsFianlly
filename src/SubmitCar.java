051f13ebe5af0e65d60fbabeaf101a211af39f68
anton panizovich <anton_panizovich@tut.by>
2022-05-16 15:34:49 +0300

    ucking done! more or less

--- src/SubmitCar.java
+++ src/SubmitCar.java
@@ -1,7 +1,10 @@
+import src.EntityNotFoundException;
+
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class SubmitCar {
-    public static void main(String[] args) throws AntonCustomException {
+    public static void main(String[] args) throws EntityNotFoundException {
         Car carSeat = new Car("seat", "cordoba", 1.6);
         Car carALfaRomeo = new Car("alfa romeo", "gulietta", 1.4);
         Car carFiat = new Car("fiat", "doblo", 1.6);
@@ -11,18 +14,25 @@ public class SubmitCar {
         String searchName1 = console1.nextLine();
 
         Car[] carList = new Car[]{carSeat, carALfaRomeo, carFiat};
-        boolean confirmationValue = true;
+        boolean confirmationValue = false;
         for (Car overSearch : carList) {
            // int check = searchName1.compareTo(overSearch.getCarMark());
             // System.out.println(check);
-           if (searchName1.equals(overSearch.getCarMark())){confirmationValue = false;}
+           if (searchName1.equals(overSearch.getCarMark())){confirmationValue = true;
+           break;}
         }
-        System.out.println(confirmationValue);
-        if (confirmationValue = true){throw new AntonCustomException( searchName1 + "  has not been found");}
+        if (confirmationValue){
+            System.out.println("we got it!");}
+        else{ try {
+                    throw new EntityNotFoundException("no match", 4);
+        }catch (RuntimeException e){
 
-
-        }
-}
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getClass());
+                    };
+    }
+}}
 
 
 
